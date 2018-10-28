21b4f8ac513eeba3431271d862e028bede15f116
Asad_Farooqi <asadullah.itcgcs@gmail.com>
2018-10-28 08:06:37 +0500

    second commit insertion sort

---
+++ 02_Getting_Started/2.1_instertion_sort.py
@@ -0,0 +1,13 @@
+def insertion_sort(array_):
+	for i in range(1, len(array_)):
+		key = array_[i]
+		j = i-1
+		while j > -1 and array_[j] > key:
+			array_[j+1] = array_[j]
+			j -= 1
+		array_[j+1] = key
+
+	print(array_)
+
+if __name__ == '__main__':
+	insertion_sort([5, 2, 4, 6, 1, 3])
