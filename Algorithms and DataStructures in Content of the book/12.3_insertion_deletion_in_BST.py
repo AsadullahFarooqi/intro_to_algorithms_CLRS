6c22bf446835ed93343cb4b635fadfb6a72ae235
AsadLiam <asadullah.itcgcs@gmail.com>
2018-11-13 13:10:31 +0500

    some other Algs and DS are added but i am still working on it

---
+++ Algorithms and DataStructures in Content of the book/12.3_insertion_deletion_in_BST.py
@@ -0,0 +1,19 @@
+
+class Node:
+	def  __init__(self, cost):
+		self.parent = None
+		self.cost = cost
+		self.left = None
+		self.right = None
+		return self.cost
+
+
+# def tree_insert(tree_root_node, new_node):
+# 	y = None
+# 	x = tree_root_node 
+# 	while x:
+# 		y = x
+# 		if z.key
+
+r = Node(55)
+print(r)
